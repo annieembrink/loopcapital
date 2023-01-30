4c8804c133483c9efcf448d3315ad7e2281b62c7
ahoijer <hoijer.amanda@gmail.com>
2023-01-30 14:57:51 +0100

    added global.css

--- loop_capital/src/components/CarouselAboutComponent.js
+++ loop_capital/src/components/CarouselAboutComponent.js
@@ -1,5 +1,6 @@
 import Carousel from 'react-bootstrap/Carousel';
 
+
 const CarouselAboutComponent = () => {
     return (
         <>
--- loop_capital/src/pages/_app.js
+++ loop_capital/src/pages/_app.js
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import '@/styles/home.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
