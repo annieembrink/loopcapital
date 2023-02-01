6595fef2f1359fa78467578621f321539f02b282
ahoijer <hoijer.amanda@gmail.com>
2023-02-01 09:13:09 +0100

    added changes

--- loop_capital/src/pages/_app.js
+++ loop_capital/src/pages/_app.js
@@ -1,7 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '@/styles/globals.css'
 import '@/styles/home/home.css'
-import '@/styles/styles.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import '@/styles/styles.css'
 
 
 export default function App({ Component, pageProps }) {
