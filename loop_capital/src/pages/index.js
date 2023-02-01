a76bdeb4d2b7d6a832570b2076763c639644b695
annalindqvist <annalindqvist_@live.se>
2023-02-01 10:29:50 +0100

    quote done ready for styling

--- loop_capital/src/pages/index.js
+++ loop_capital/src/pages/index.js
@@ -30,6 +30,18 @@ export default function Home(props) {
       </ul>
     </div>
 
+    <div>
+      {/* Add quote icon */}
+      <div>"QUOTE"</div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        {/* Right align text */}
+        <p>Name Namesson</p>
+        <p>Position</p>
+    </div>
+
     {/* MOVE THIS TO COMPONENT? */}
       {props.wpDataJson.map(num => 
       <div key={num}>
