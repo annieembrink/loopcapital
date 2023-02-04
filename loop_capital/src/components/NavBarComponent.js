c9a4aeed4957f1218503491361fd40084f3b0868
Lizeys <susanne-karlsen@outlook.com>
2023-02-04 23:21:46 +0100

    started navbar

--- loop_capital/src/components/NavBarComponent.js
+++ loop_capital/src/components/NavBarComponent.js
@@ -1,24 +1,40 @@
-import Link from 'next/link'
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
-const NavBarComponent = () => {
-    return (
-        <>
-        <nav>
-            <ul>
-                <li>
-                    <Link href="/">Home</Link>
-                </li>
-                <li>
-                    <Link href="/portfolio">Portfolio</Link>
-                </li>
-                
-                <li>
-                    <Link href="/contact">Contact</Link>
-                </li>
-            </ul>
-        </nav>
-        </>
-    );
+import Image from 'next/image'
+
+
+function NavBarComponent() {
+  return (
+    <>
+     <Navbar bg="none" expand="lg" fixed="top" variant="dark">
+      <Container fluid>
+        <Navbar.Brand href="/">
+        <Image width={50} height={50} src='/images/loop_capital.png' className='logo' alt='loopcapital'/>
+</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll"  />
+        <Navbar.Collapse id="navbarScroll" className="end">
+          <Nav
+            className="me-end my-2 my-lg-0 end"
+            style={{ maxHeight: '200px' }}
+            navbarScroll
+          >
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+           
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+          
+    </>
+  );
 }
 
 export default NavBarComponent;
