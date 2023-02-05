8a79b4f7c45c2826a0370cc7d457fc59813fb08f
ahoijer <hoijer.amanda@gmail.com>
2023-02-05 10:37:00 +0100

    footer_finished

--- loop_capital/src/components/NavBarComponent.js
+++ loop_capital/src/components/NavBarComponent.js
@@ -12,27 +12,27 @@ import Image from 'next/image'
 function NavBarComponent() {
   return (
     <>
-     <Navbar bg="none" expand="lg" fixed="top" variant="dark">
-      <Container fluid>
-        <Navbar.Brand href="/">
-        <Image width={50} height={50} src='/images/loop_capital.png' className='logo' alt='loopcapital'/>
-</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll"  />
-        <Navbar.Collapse id="navbarScroll" className="end">
-          <Nav
-            className="me-end my-2 my-lg-0 end"
-            style={{ maxHeight: '200px' }}
-            navbarScroll
-          >
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/portfolio">Portfolio</Nav.Link>
-            <Nav.Link href="/contact">Contact</Nav.Link>
-           
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-          
+      <Navbar bg="none" expand="lg" fixed="top" variant="dark">
+        <Container fluid>
+          <Navbar.Brand href="/">
+            <Image width={70} height={70} src='/images/loop_capital.png' className='logo-header' alt='loopcapital' />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll" className="end">
+            <Nav
+              className="me-end my-2 my-lg-0 end"
+              style={{ maxHeight: '200px' }}
+              navbarScroll
+            >
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+              <Nav.Link href="/contact">Contact</Nav.Link>
+
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
     </>
   );
 }
