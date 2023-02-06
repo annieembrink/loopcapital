614586547a721163913469f31cbcd51696a1a74a
Lizeys <susanne-karlsen@outlook.com>
2023-02-06 13:21:35 +0100

    added activeClass in Nav

--- loop_capital/src/components/NavBarComponent.js
+++ loop_capital/src/components/NavBarComponent.js
@@ -1,45 +1,58 @@
-
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 
-import Image from 'next/image'
+import Image from 'next/image';
 import { Icon } from '@iconify/react';
-import Link from "next/link";
+
+import { useRouter } from 'next/router';
+// import Link from "next/link";
+// import "bootstrap/dist/js/bootstrap.bundle.min";
 
 
 function NavBarComponent() {
+  const router = useRouter();
+const currentRoute = router.pathname;
+  
   return (
     <>
-      <Navbar bg="none" expand="lg" fixed="top" variant="dark">
-        <Container fluid>
-          <Navbar.Brand href="/">
-            <Image width={70} height={70} src='/images/loop_capital.png' className='logo-header' alt='loopcapital' />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse  id="navbarScroll" className="end mobilemenu">
-            <Nav
-              className="me-end my-2 my-lg-0 end mobilemenu_link"
-              // style={{ height: '100vw', width: '100vh' }}
-              navbarScroll
-             
+      {['lg'].map((expand) => (
+        <Navbar key={expand} bg="none" expand={expand} variant="dark" className="mb-3" fixed="top">
+          <Container fluid>
+            <Navbar.Brand href="/"><Image width={70} height={70} src='/images/loop_capital.png' className='logo-header' alt='loopcapital' /></Navbar.Brand>
+            <Navbar.Toggle className="hamburger_icon" aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              className="offcanvas_container"
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
             >
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/portfolio">Portfolio</Nav.Link>
-              <Nav.Link href="/contact">Contact</Nav.Link>
-              <Nav.Link href="https://www.linkedin.com/company/loop-capital-ab/?trk=similar-pages"><Icon icon="akar-icons:linkedin-box-fill" width="25" className="mobilemenu_icon"/></Nav.Link>
+              <Offcanvas.Header closeButton className="offcanvas_header">
+                <Offcanvas.Title href="/" id={`offcanvasNavbarLabel-expand-${expand}`}>
+                <Image width={70} height={70} src='/images/loop_capital.png' className='logo-header' alt='loopcapital' />
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3 offcanvas_container_link">
+                
+                  <Nav.Link href="/" className={currentRoute === "/" ? "active"  : ""}>Home</Nav.Link>
+                  <Nav.Link href="/portfolio" className={currentRoute === "/portfolio" ? "active"  : ""}>Portfolio</Nav.Link>
+                  <Nav.Link href="/contact" className={currentRoute === "/contact" ? "active"  : ""}>Contact</Nav.Link>
+                  <Nav.Link href="https://www.linkedin.com/company/loop-capital-ab/?trk=similar-pages"><Icon icon="akar-icons:linkedin-box-fill" width="25" className="mobilemenu_icon"/></Nav.Link>
 
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-
+                  
+                </Nav>
+               
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
     </>
   );
 }
 
 export default NavBarComponent;
+
+
