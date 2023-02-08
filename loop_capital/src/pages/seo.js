d713b23e7917777ef13f710d2a77c1271e4e3d04
anniesandberg <90192959+anniesandberg@users.noreply.github.com>
2023-02-08 09:58:58 +0100

    meta title description and img added from wp api

--- loop_capital/src/pages/seo.js
+++ loop_capital/src/pages/seo.js
@@ -25,7 +25,7 @@ export async function getStaticProps({ preview = false }) {
     // let wpData = await fetch('https://172-104-145-53.ip.linodeusercontent.com/#/schema/logo/image/');
     let wpData = await fetch('https://172-104-145-53.ip.linodeusercontent.com/wp-json/yoast/v1/get_head?url=https://172-104-145-53.ip.linodeusercontent.com/#/schema/Organization/1');
     // let wpData = await fetch('https://172-104-145-53.ip.linodeusercontent.com/wp-json/yoast/v1/get_head?url=https://172-104-145-53.ip.linodeusercontent.com/');
-    // let wpDataJson = await wpData.json()
+    let wpDataJson = await wpData.json()
     // console.log(wpDataJson)
   
     return {
