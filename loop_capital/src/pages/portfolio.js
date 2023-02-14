35cfd9aeed14b372d82f3ef722dcc9f870c1e4bd
ahoijer <hoijer.amanda@gmail.com>
2023-02-14 08:35:49 +0100

    final final final

--- loop_capital/src/pages/portfolio.js
+++ loop_capital/src/pages/portfolio.js
@@ -81,7 +81,7 @@ export default function Portfolio(props) {
                   <Card.Img variant="top" src={company.acf.image_of_the_company_logo} />
                   <Card.Body className="card-body">
                     <Card.Title className="h3">{company.title.rendered}</Card.Title>
-                    <Card.Text className="green-text h4 roboto-font">{company.acf.branch}</Card.Text>
+                    <Card.Text className="green-text roboto-font">{company.acf.branch}</Card.Text>
                   </Card.Body>
                 </Card>
               </Col>
